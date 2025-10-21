e2a9379954bb814cf04b81632cfc4a0ec21fd7f9
Nady Moura <nadymouradesenvolvedoraweb@gmail.com>
2025-10-21 17:18:38 -0300

    Add files via upload

---
+++ back-end(Nady)/aprovacao.py
@@ -0,0 +1,5 @@
+nota = 9
+if nota >= 7:
+    print("Aprovado!")
+else:
+    print("Reprovado!")
---
+++ back-end(Nady)/aula2.py
@@ -0,0 +1,44 @@
+# criar uma função para calcular a area de um circulo
+'''
+def circulo(raio):
+    pi = 3.1415926
+    area = pi * (raio**2)
+    return area
+
+x = int(input())
+print("Digite um valor do raio para calcularmos o valor da area do circulo" + circulo(x))
+'''
+
+'''
+def aplicar_desconto(preco, percentual):
+    desconto = preco * (percentual / 100)
+    preco_final = preco - desconto
+    return preco_final
+
+print(aplicar_desconto(80, 10))
+'''
+
+'''
+def celsius_para_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+x = int(input())
+print(celsius_para_fahrenheit(x))
+'''
+
+'''
+idade = 16
+if idade >=18:
+    print("pode dirigir")
+else:
+    print("ainda não pode dirigir")
+'''
+
+'''
+nota = 8
+if nota >= 9:
+    print("Exelente!)
+elif nota >= 7:
+    print("Bom!")
+'''
